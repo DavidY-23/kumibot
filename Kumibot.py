47a0da8ce920e5e41f7a8e40db50c86e57120c73
unknown <davidye0201@gmail.com>
2021-05-01 03:00:13 -0400

    keys again

--- Kumibot.py
+++ Kumibot.py
@@ -5,10 +5,10 @@ import random
 
 from os import environ 
 
-consumer_key =        environ['consumer_key']
-consumer_secret =     environ['consumer_secret']
-access_token = environ['access_key']
-access_token_secret =  environ['access_token_secret']
+consumer_key = environ['consumer_key']
+consumer_secret = environ['consumer_secret']
+access_token = environ['access_token']
+access_token_secret = environ['access_token_secret']
 
 #credentials for Twitter
 
