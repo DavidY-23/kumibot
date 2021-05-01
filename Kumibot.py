c26df5627f771f1b020e9d9c165a6060daeb7218
unknown <davidye0201@gmail.com>
2021-05-01 02:10:37 -0400

    yes

---
+++ Kumibot.py
@@ -0,0 +1,20 @@
+import tweepy as tp
+import time
+import os
+import random
+import config
+
+#credentials for Twitter
+
+auth = tp.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_token_secret)
+api = tp.API(auth)
+
+def func():
+    os.chdir('Kumiko')
+    for kumiko in os.listdir('.'):
+        #api.update_status("Kumiko.")
+        kumiko = random.choice(os.listdir('.'))
+        api.update_with_media(kumiko, status="Kumiko Oumae")
+        time.sleep(1800)
+func()
